4b3750a8b01210be11720e3e81bd5b09a20497d0
peticka <petia_radeva@yahoo.com>
2014-07-21 21:55:10 +0300

    home page

---
+++ home.php
@@ -0,0 +1,59 @@
+<?php 
+	require_once('header.php');
+?>
+
+						<h2><br> Nai- dobriq online magazin za knigi! Tuk moje da namerite nai- novite knigi i vechnite bestselyri na vsichki vremena</h2>
+				
+		
+			<section>
+			
+				<div class="column">	
+					<img src="kniga1.jpg">
+					<div class="productsText">
+						<h3>Tymnata strana na lubovta</h3>
+						<p><h3>Cena: 16<sup> 00 </sup> lv. </h3></p>
+					</div>
+				
+				
+					
+					<img src="kniga2.jpg">
+					<div class="productsText">
+						<h3>Dante's Antichthon<h3>
+						<p>Cena: 18<sup> 90 </sup> lv.</p>
+					</div>
+					</div>
+				
+				<br>
+				<br>
+					<div class="border">
+					<img src="vk.jpg" >
+					<div class="productsText">
+						 <h3>The Fault in Our Stars
+			  <br><cite> John Green </h3></cite>
+						<p> <h3>Cena: 15<sup> 50 </sup> lv. </h3></p>
+				
+				
+				
+					<img src="html.jpg">
+					<div class="productsText">
+						<h3>HTML 5 & CSS 3</h3>
+						<p><h3> Cena: 19<sup> 95 </sup> lv.</h3>	</p>
+					</div>
+				
+				<div class="spacer"></div>
+			</section>
+			<section class="latestNews">
+				<article>
+					<h1>Latest News</h1>
+					<p>
+						Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam ... <a href="">Read More</a>
+					</p>
+					<p>
+						Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam ... <a href="">Read More</a>
+					</p>
+					<p>
+						Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam ... <a href="">Read More</a>
+					</p>
+				</article>
+			</section>
+<?php require_once('footer.php'); ?>
